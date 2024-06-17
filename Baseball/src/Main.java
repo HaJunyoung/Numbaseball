d7549f8fcd8c6f614b87ec1378d8806eef31fe65
Ha Junyoung <96538008+HaJunyoung@users.noreply.github.com>
2024-06-17 19:46:55 +0900

    Update Main.java

--- Baseball/src/Main.java
+++ Baseball/src/Main.java
@@ -1,62 +1,91 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
 
-        int[] myNum =  new int[3];
+    static int strike;
+    static int ball;
+    static int count;
+
+    static void resetGame(){
+        strike = 0;
+        ball = 0;
+        count = 1;
+    }
+
+    static int[] makeNum(){
         int[] comNum = new int[] {
                 (int)(Math.random() * 9) + 1,
                 (int)(Math.random() * 9) + 1,
                 (int)(Math.random() * 9) + 1,
-                         };
-
-        System.out.println("정답 : " + Arrays.toString(comNum));
-
-        int strike = 0;
-        int ball = 0;
-        int count = 1;
+        };
+        return comNum;
+    }
+    static int[] userNum(){
         Scanner input = new Scanner(System.in);
+        int[] myNum = new int[3];
 
-        System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
-        while (true) {
-            //사용자 숫자 입력
-            for (int i = 0; i < comNum.length; i++) {
-                int tempNum;
-                do {
-                    tempNum = input.nextInt();
-                    input.nextLine();
-                    if (tempNum > 9) {
-                        System.out.println("입력값을 1~9까지로 입력해주세요");
-                    }
-                } while (tempNum >= 10);
+        //사용자 숫자 입력
+        for (int i = 0; i < 3; i++) {
+            int tempNum = input.nextInt();
+            if(tempNum < 1 || tempNum > 9) {
+                System.out.println("입력값을 1~9까지로 입력해주세요");
+                i--;
+            }
+            else{
+                input.nextLine();
                 myNum[i] = tempNum;
+            }
         }
-
-            // 검증
-            for (int i = 0 ; i<comNum.length ; i++){
-                for(int a=0; a<myNum.length ; a++){
-                    if(comNum[i] == myNum[a] && i == a) {
-                        //자리 확인
-                        strike++;
-                    }else if(comNum[i] == myNum[a] && i != a) {
-                        //같은 숫자만 있으면 볼
-                        ball++;
-                    }
+        return myNum;
+    }
+    static void compareNum(int[] comNum, int[] myNum){
+        for (int i = 0 ; i<comNum.length ; i++){
+            for(int a=0; a<myNum.length ; a++){
+                if(comNum[i] == myNum[a] && i == a) {
+                    //자리 확인
+                    strike++;
+                }else if(comNum[i] == myNum[a] && i != a) {
+                    //같은 숫자만 있으면 볼
+                    ball++;
                 }
             }
+        }
+    }
+
+    static void countCheck(int[] myNum){
+        System.out.print(count + "번째 시도 : ");
+        for (int x : myNum) {
+            //내숫자 확인
+            System.out.print(x);
+        }
+        System.out.println();
+        System.out.println(ball + "B" + strike + "S");
+        count ++;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Strike , ball , count 초기화
+        resetGame();
+
+        // 컴퓨터 랜덤 3자리 만듦
+        int[] comNum = makeNum();
+        System.out.println("정답 : " + Arrays.toString(comNum));
+        System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
+
+        while (true) {
+
+            //사용자 숫자 입력
+            int[] myNum= userNum();
+
+            // 검증
+            compareNum(comNum,myNum);
 
             // 출력
-            String newMyNum = Arrays.toString(myNum);
-            System.out.print(count + "번째 시도 : ");
-            for (int x : myNum) {
-                //내숫자 확인
-                System.out.print(x);
-            }
-            System.out.println();
-            System.out.println(ball + "B" + strike + "S");
-            count ++;
+            countCheck(myNum);
+
 
             // 종료 조건
             if (strike == 3) {
@@ -65,9 +94,6 @@ public class Main {
                 break;
             }
 
-            //초기화
-            strike = 0;
-            ball = 0;
         }
     }
 }
